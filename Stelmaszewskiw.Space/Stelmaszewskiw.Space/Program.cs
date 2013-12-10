04720e31fa0f5553e2ed3235f927d672645d42c4
Wojciech Stelmaszewski <stelmaszewskiw@gmail.com>
2013-12-10 20:42:04 +0100

    Visual Studio 2012 project added.

---
+++ Stelmaszewskiw.Space/Stelmaszewskiw.Space/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Stelmaszewskiw.Space
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var program = new Space())
+            {
+                program.Run();
+            }
+        }
+    }
+}
---
+++ Stelmaszewskiw.Space/Stelmaszewskiw.Space/Space.cs
@@ -0,0 +1,39 @@
+﻿using SharpDX;
+using SharpDX.Toolkit;
+
+namespace Stelmaszewskiw.Space
+{
+    public class Space : Game
+    {
+        private readonly GraphicsDeviceManager _graphicDeviceManager;
+
+        public Space()
+        {
+            _graphicDeviceManager = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Initialize()
+        {
+            Window.Title = "Space by Wojciech Stelmaszewski (2013)";
+
+            IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
